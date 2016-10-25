4cf7cfbf29ef8dfeb63f51a10518eb06c560a307
leokhachatorians <leokhachatorians@gmail.com>
2016-10-25 11:33:32 -0700

    Adjusted comments

--- chapter3/sum2.s
+++ chapter3/sum2.s
@@ -12,19 +12,19 @@ myvar2:
 .balign 4
 .global main
 main:
-	ldr 	r1, addr_of_myvar1 // r1 <- &myvar1
-	mov	r3, #3
-	str 	r3, [r1]
+	ldr 	r1, addr_of_myvar1 	// r1 = &myvar1
+	mov	r3, #3			// r3 = 3
+	str 	r3, [r1] 		// *r1 = r3
 
-	ldr 	r2, addr_of_myvar2 // r2 <- &myvar2
-	mov 	r3, #4
-	str 	r3, [r2]
+	ldr 	r2, addr_of_myvar2 	// r2 = &myvar2
+	mov 	r3, #4 			// r3 = 4
+	str 	r3, [r2] 		// *r2 = r3
 
-	ldr 	r1, addr_of_myvar1
-	ldr 	r1, [r1]
+	ldr 	r1, addr_of_myvar1	// r1 = &myvar1
+	ldr 	r1, [r1]		// r1 = *r1
 
-	ldr 	r2, addr_of_myvar2
-	ldr 	r2, [r2]
+	ldr 	r2, addr_of_myvar2	// r2 = &myvar2
+	ldr 	r2, [r2]		// r2 = *r2
 
 	add 	r0, r1, r2
 	bx 	lr
