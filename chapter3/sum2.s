0353ac5a3760b098b8c4888719e98cb5985c3f21
leokhachatorians <leokhachatorians@gmail.com>
2016-10-25 11:12:30 -0700

    Setting variables to 0

---
+++ chapter3/sum2.s
@@ -0,0 +1,24 @@
+.data
+
+.balign 4
+myvar1:
+	.word 0
+
+.balign 4
+myvar2:
+	.word 0
+
+.text
+.balign 4
+.global main
+main:
+	ldr 	r1, addr_of_myvar1 // r1 <- &myvar1
+	mov	r3, #3
+	str 	r3, [r1]
+
+	ldr 	r2, addr_of_myvar2 // r2 <- &myvar2
+	mov 	r3, #4
+	str 	r3, [r2]
+
+addr_of_myvar1 : .word myvar1
+addr_of_myvar2 : .word myvar2
