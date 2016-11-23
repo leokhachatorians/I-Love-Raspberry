0345d3298f23140990ba35b669ada8d0eaa67620
Leo Khachatorians <leokhachatorians@gmail.com>
2016-11-22 23:20:58 -0800

    Fixed typos mostly

--- project_euler/problem1.s
+++ project_euler/problem1.s
@@ -15,17 +15,15 @@ main:
 	MOV	r3, #0		// flag if divisible by 5
 	MOV 	r6, #0		// running sum of all divisible nums	
 
-	MOV	r1, #0
-	MOV 	r4, r1 		// keep copy of number in r4
+	MOV	r1, #0		// set r1 to zero
 
 incrementer:
-	ADD 	r1, #1
+	ADD 	r1, #1		// increment r1
 	MOV 	r4, r1 		// keep copy of number in r4
-	
 
 division_loop_3:
-	SUB	r1, #3
-	CMP	r1, #0
+	SUB	r1, #3		// subtract by 3
+	CMP	r1, #0		// compare with zero
 	BEQ	set_flag_3_true
 	BLE	set_flag_3_false
 	B	division_loop_3
@@ -37,25 +35,25 @@ set_flag_3_true:
 	B	division_loop_5
 
 set_flag_3_false:
-	MOV	r2, #0  	// set divisble by 3 flag false
+	MOV	r2, #0  	// set divisible by 3 flag false
 	MOV	r1, r4		// load number back into r1
 
 division_loop_5:
-	SUB	r1, #5
-	CMP	r1, #0
+	SUB	r1, #5		// subtract by 5
+	CMP	r1, #0		// compare with 0
 	BEQ	set_flag_5_true
 	BLE	set_flag_5_false
 	B	division_loop_5
 
 set_flag_5_true:
-	MOV	r3, #1  	// set divisble by 5 flag true
+	MOV	r3, #1  	// set divisible by 5 flag true
 	MOV	r1, r4		// load number back into r1
-	CMP	r2, #0		// check if divisble by 3 flag is set
+	CMP	r2, #0		// check if divisible by 3 flag is set
 	ADDEQ	r6, r6, r1	// add to our sum if it isnt set
 	B	end
 
 set_flag_5_false:
-	MOV	r3, #0		// set divisble by flag 5 false
+	MOV	r3, #0		// set divisible by flag 5 false
 	MOV	r1, r4		// load number back into r1
 
 end:
@@ -63,6 +61,6 @@ end:
 	CMP	r1, r8		// and I just compare it this way :|
 	BNE	incrementer
 	MOV	r1, r6		// load r6 (the total sum) in to r1 for printf purposes
-	LDR	r0, =total_sum
+	LDR	r0, =total_sum	// load address of our string into display register
 	BL	printf
 	POP	{PC}
